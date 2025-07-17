b90506311864396c546ceb204756867cb9c17696
Palapol0025 <6611130025@mut.ac.th>
2025-07-17 16:46:20 +0700

    commit1

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+    echo "1111111111111111111";
+    echo "55555555555555555555";
+    echo "This subject is Git Tutariai!!";
+?>
---
+++ menu.php
@@ -0,0 +1,5 @@
+<?php
+    echo "pppppppppppp";
+    echo "tttttttttttt";
+    echo "tttttttttttt";
+?>
